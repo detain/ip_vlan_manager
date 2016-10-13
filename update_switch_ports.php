4dfc4c21b745a5a5673f01ffb4d24dd5bdb73b0f
detain <detain@b7b71f07-cbf7-474a-b9d0-0f5a9ada8a20>
2016-10-13 22:24:58 +0000

    adding 2 files that have been a part of the whole vlan thing but not really in svn yet

---
+++ update_switch_ports.php
@@ -0,0 +1,118 @@
+#!/usr/bin/php -q
+<?php
+	/************************************************************************************\
+	* Trouble Free Admin                                                                 *
+	* (c)2002-2004 Interserver                                                           *
+	* ---------------------------------------------------------------------------------- *
+	* Description: find / fix common billing problems                                    *
+	\************************************************************************************/
+
+	// $Id: generate_banks_table.php,v 1.1.1.1 2007-04-14 22:47:56 detain Exp $
+	// $Source: /var/lib/cvs/troublefree/tempscripts/generate_banks_table.php,v $
+	
+	// accounts (id, login, pass, groups)
+	// accounts_ext (id, key, value)
+	
+	define('DEBUG',FALSE);
+	
+	// Load Various Functions & Connect to mysql database
+	include('../include/functions.inc.php');
+	ob_end_flush();
+	$db = $GLOBALS['tf']->db;
+	$db2 = $db;
+	
+	$lines = explode("\n", trim(`curl -s http://nms.interserver.net/cac/servermap.php`));
+	$switches = array();
+	foreach ($lines as $line)
+	{
+		$parts = explode(',', $line);
+		list($graph, $switch, $port, $comment) = $parts;
+		if ($switch != '')
+		{
+			$switches[$switch][$port] = $graph;
+		}
+	}
+	foreach ($switches as $switch => $ports)
+	{
+		$foundports = array();
+		$db->query("select * from switchmanager where name='$switch'");
+		if ($db->num_rows() > 0)
+		{
+			$db->next_record();
+			$row = $db->Record;
+			echo "Loaded Switch $switch - ";
+		}
+		else
+		{
+			$db->query("insert into switchmanager values (NULL, '$switch', " . sizeof($ports) . ")");
+			$db->query("select * from switchmanager where name='$switch'");
+			$db->next_record();
+			$row = $db->Record;
+			echo "Created New Switch $switch - ";
+		}
+		$id = $row['id'];
+		foreach ($ports as $port => $graph)
+		{
+			$blade = '';
+			$justport = $port;
+			if (strrpos($port, '/') > 0)
+			{
+				$blade = substr($port, 0, strrpos($port, '/'));
+				$justport = substr($port, strlen($blade) + 1);
+			}
+			if (isset($foundports[$justport]))
+			{
+				$justport = '';
+			}
+			else
+			{
+				$foundports[$justport] = true;
+			}
+			$db->query("select * from switchports where switch='$id' and port='$port'");
+			if ($db->num_rows() == 0)
+			{
+				echo "$port +";
+				$db->query("insert into switchports values ('$id', '$blade', '$justport', '$port', '$graph', '')");
+			}
+			else
+			{
+				$db->next_record();
+				if (($db->Record['blade'] != $blade) || ($db->Record['justport'] != $justport))
+				{
+echo "\nUpdate BladePort";
+					$query = "update switchports set blade='$blade', justport='$justport' where switch='$id' and port='$port'";
+					//echo $query;
+					$db->query($query);
+				}
+				echo "$port ";
+				if ($db->Record['graph_id'] != $graph)
+				{
+echo "\nUpdate Graph";
+					$query = "update switchports set graph_id='$graph' where switch='$id' and port='$port'";
+					//echo $query;
+					$db->query($query);
+				}
+				echo "$graph ";
+			}
+			$query = "select * from vlans where vlans_ports like '%:$row[id]/$justport:%' or vlans_ports like '%:$row[id]/$port:%'";
+			//echo "$query\n";
+			$db->query($query);
+			$vlans = array();
+			while ($db->next_record())
+			{
+				$vlans[] = $db->Record['vlans_id'];
+			}
+			if (sizeof($vlans) > 0)
+			{
+				echo "(" . sizeof($vlans) . " Vlans)";
+				$vlantext = implode(',', $vlans);
+				$db->query("update switchports set vlans='$vlantext' where switch='$id' and port='$port'");
+				if ($db->affected_rows())
+					echo "\nUpdate Vlan";
+			}
+			echo ",";
+		}
+		echo "\n";
+	}
+//	print_r($switches);
+?>
---
+++ vlantorwhois.php
@@ -0,0 +1,420 @@
+<?php
+	// Load Various Functions & Connect to mysql database
+	include('../include/functions.inc.php');
+	include('../include/ip.functions.inc.php');
+	$db = $GLOBALS['tf']->db;
+	$db4 = $db;
+	$db_innertell = get_module_db('innertell');
+	$db3 = $db_innertell;
+	$db_mb            = get_module_db('mb');
+	ob_end_flush();
+
+
+// set extra to a:1:{s:13:"private_whois";s:1:"1";}
+function get_extra($extra)
+{
+    if ($extra == '')
+        $extra = array();
+    else
+        $extra = unserialize($extra);
+    return $extra;
+}
+
+function put_extra($extra)
+{
+    if ($extra == '')
+        $extra = array();
+    $extra = serialize($extra);
+    return $extra;
+}
+
+	$db->query("select * from ipblocks");
+	$ipblocks = array();
+	while ($db->next_record())
+	{
+		$ipblocks[$db->Record['ipblocks_id']] = $db->Record['ipblocks_network'];
+	}
+
+	$db->query("select * from vlans left join ipblocks on ipblocks_id=vlans_block where vlans_comment != 'REUSE' and vlans_id is not null");
+	$total = 0;
+	while ($db->next_record())
+	{
+		$found = false;
+		if (!isset($ipblocks[$db->Record['vlans_block']]))
+		{
+			error_log("Skipping VLAN {$db->Record['vlans_id']}, probably not our IPS");
+			continue;		
+		}
+		else
+		{
+			$ipblock = $ipblocks[$db->Record['vlans_block']];
+		}
+		$ipblockdata = explode('/', $ipblock);
+		$ipblock_ip = $ipblockdata[0];
+		$ipblock_size = $ipblockdata[1];
+		$vlan = str_replace(':', '', $db->Record['vlans_networks']);
+		$vlandata = explode('/', $vlan);
+		$network_info = ipcalc($vlan);
+		$maxip = $network_info['broadcast'];
+		$ip = $vlandata[0];
+		$size = $vlandata[1];
+		$total += trim(`/usr/local/bin/ipcalc -n $ip/$size | grep Hosts | cut -d" " -f2`);
+		$cmds = '';
+		$server = trim(str_replace(
+array('(FIXME Port Unknown)', 'FIXME PORT WRONG', 'FIX ME PORT WRONG', 'reuse', 'append', '[ip transit]'), 
+array('', '', '', '', '', ''),
+ereg_replace('\(.*\)', '', strtolower($db->Record['vlans_comment']))));
+//echo "\nServer: $server :";
+		echo "echo \"VLAN $vlan SERVER $server\";\n";
+		if (preg_match("/^mb(\d)*$/", $server))
+		{
+/*
+select * from users where id=2311;
++----------------+---------+---------+-------+--------+----------+---------------------------+--------------------+------+---------------------+-------------+----------+------+---------------+
+| phone          | zipcode | country | state | city   | address2 | address1                  | company            | id   | username            | name        | password | type | modernbill_id |
++----------------+---------+---------+-------+--------+----------+---------------------------+--------------------+------+---------------------+-------------+----------+------+---------------+
+| (559) 892-0902 | 93720   | usa     | ca    | fresno |          | 7969 N Blackstone Av #248 | prominent upstairs | 2311 | greg@truecanyon.com | greg lontok | wide12   | NULL |          2427 |
+*/
+			$db3->query("select * from users where id='" . str_replace('mb', '', $server) . "'");
+			$db3->next_record();
+				$data = $db3->Record;
+			$extra = get_extra($data['extra']);
+				if ($data['address1'] == '')
+				{
+					$data['address1'] = 'na';
+				}
+				if ($data['city'] == '')
+				{
+					$data['city'] = 'na';
+				}
+				if ($data['state'] == '')
+				{
+					$data['state'] = 'na';
+				}
+				if ($data['country'] == '')
+				{
+					$data['country'] = 'US';
+				}
+				if (trim($data['company']) == '')
+				{
+					$org = $data['name'];
+				}
+				else
+				{
+					$org = $data['company'];
+				}
+				if (trim($org) == '')
+				{
+					$org = 'Account' . $data['id'];
+				}
+				if ($extra['private_whois'] == 1)
+				{
+//					$org = 'Private Customer';
+//					$data['address1'] = 'Private Residence';
+					
+				}
+				$found = true;
+				$cmds .= 'cd /opt/rwhoisd/etc/rwhoisd/net-' . $ipblock_ip . '-' . $ipblock_size . ';\n'
+				. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+				. 'Auth-Area: ' . $ipblock . '\n'
+				. 'Org-Name: ' . trim($org) . '\n'
+				. 'Street-Address: ' . trim($data['address1']) . '\n'
+				. 'City: ' . $data['city'] . '\n'
+				. 'State: ' . $data['state'] . '\n'
+				. 'Postal-Code: ' . $data['zip'] . '\n'
+				. 'Country-Code: ' . $data['country'] . '\n'
+				. 'Phone: ' . trim($data['phone']) . '\n'
+				. 'Created: 20050101\n'
+				. 'Updated: ' . date('Ymd') . '" > data/org/' . $data['id'] . '.txt;\n'
+				. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+				. 'Auth-Area: ' . $ipblock . '\n'
+				. 'Network-Name: INTSRV-' . $ip . '\n'
+				. 'IP-Network: ' . $ip . '/' . $size . '\n'
+				. 'Org-Name: ' . trim($org) . '\n'
+				. 'Street-Address: ' . $data['address1'] . '\n'
+				. 'City: ' . $data['city'] . '\n'
+				. 'State: ' . $data['state'] . '\n'
+				. 'Postal-Code: ' . $data['zip'] . '\n'
+				. 'Country-Code: ' . $data['country'] . '\n'
+				. 'Created: 20050101\n'
+				. 'Updated: ' . date('Ymd') . '\n'
+				. 'Updated-By: abuse@interserver.net" > data/network/' . $ip . '-' . $size . '.txt;\n';
+		}
+		else
+		{
+				$query = "select id, username, date from orders where servername like '%$server%' or servername='$server'";
+				$db_innertell->query($query);
+//echo $db_innertell->num_rows() . "|";
+				$dparts = explode('.', $server);
+				$dsize = sizeof($dparts);
+				echo "echo \"Query: $query (Rows: " . $db_innertell->num_rows() . ")\";\n";
+				if (($db_innertell->num_rows() == 0) && ($dsize > 2))
+				{
+					$server = $dparts[$dsize - 2] . '.' . $dparts[$dsize - 1];
+					$db_innertell->query("select id, username, date from orders where servername like '%$server%' or servername='$server'");
+					$drows = $db_innertell->num_rows();
+//					`echo "$server:$drows" >&2`;
+				}
+				if ($db_innertell->num_rows() > 0)
+				{
+					$db_innertell->next_record();
+					$serverinfo = $db_innertell->Record;
+
+						$db3->query("select * from users where username='" . $serverinfo['username'] . "'");
+						if ($db3->num_rows() == 0)
+						{
+							$db3->query("select * from users where id=9");
+						}
+						$db3->next_record();
+						$data = $db3->Record;
+						$extra = get_extra($data['extra']);
+
+					$query = "select * from client_info where client_email='" . $serverinfo['username'] . "'";
+					$db_mb->query($query);
+					if ($server['username'] == 'services@expressvpn.com')
+					{
+						echo $query;
+					}
+					echo "echo \"Query: $query (Rows: " . $db_mb->num_rows() . ")\";\n";
+					if ($db_mb->num_rows() > 0)
+					{
+						$db_mb->next_record();
+						$data = $db_mb->Record;
+					if ($server['username'] == 'services@expressvpn.com')
+					{
+						echo $query;
+						print_r($data);
+					}
+						if ($data['client_address'] == '')
+						{
+							$data['client_address'] = 'na';
+						}
+						if ($data['client_city'] == '')
+						{
+							$data['client_city'] = 'na';
+						}
+						if ($data['client_state'] == '')
+						{
+							$data['client_state'] = 'na';
+						}
+						if ($data['client_country'] == '')
+						{
+							$data['client_country'] = 'US';
+						}
+						if (trim($data['client_company']) == '')
+						{
+							$org = $data['client_fname'] . ' ' . $data['client_lname'];
+						}
+						else
+						{
+							$org = $data['client_company'];
+						}
+						if (trim($org) == '')
+						{
+							$org = 'Account' . $data['client_id'];
+						}
+						if ((isset($extra['private_whois']) && $extra['private_whois'] == 1) || $data['client_field_10'] == 1)
+						{
+//							$org = 'Private Customer';
+//							$data['address1'] = 'Private Residence';
+//							$data['client_address'] = 'Private Residence';
+						}
+						$found = true;
+						$cmds .= 'cd /opt/rwhoisd/etc/rwhoisd/net-' . $ipblock_ip . '-' . $ipblock_size . ';\n'
+						. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+						. 'Auth-Area: ' . $ipblock . '\n'
+						. 'Org-Name: ' . trim($org) . '\n'
+						. 'Street-Address: ' . trim($data['client_address']) . '\n'
+						. 'City: ' . trim($data['client_city']) . '\n'
+						. 'State: ' . trim($data['client_state']) . '\n'
+						. 'Postal-Code: ' . trim($data['client_zip']) . '\n'
+						. 'Country-Code: ' . trim($data['client_country']) . '\n'
+						. 'Phone: ' . trim($data['client_phone1']) . '\n'
+						. 'Created: ' . date('Ymd', $serverinfo['date']) . '\n'
+						. 'Updated: ' . date('Ymd') . '" > data/org/' . trim($data['client_id']) . '.txt;\n'
+						. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+						. 'Auth-Area: ' . $ipblock . '\n'
+						. 'Network-Name: INTSRV-' . $ip . '\n'
+						. 'IP-Network: ' . $ip . '/' . $size . '\n'
+						. 'Org-Name: ' . trim($org) . '\n'
+						. 'Street-Address: ' . trim($data['client_address']) . '\n'
+						. 'City: ' . trim($data['client_city']) . '\n'
+						. 'State: ' . trim($data['client_state']) . '\n'
+						. 'Postal-Code: ' . trim($data['client_zip']) . '\n'
+						. 'Country-Code: ' . trim($data['client_country']) . '\n'
+						. 'Created: ' . date('Ymd', $serverinfo['date']) . '\n'
+						. 'Updated: ' . date('Ymd') . '\n'
+						. 'Updated-By: abuse@interserver.net" > data/network/' . $ip . '-' . $size . '.txt;\n';
+					}
+					else
+					{
+						$db3->query("select * from users where username='" . $serverinfo['username'] . "'");
+						if ($db3->num_rows() == 0)
+						{
+							$db3->query("select * from users where id=9");
+						}
+						$db3->next_record();
+						$data = $db3->Record;
+						$extra = get_extra($data['extra']);
+						if ($data['address1'] == '')
+						{
+							$data['address1'] = 'na';
+						}
+						if ($data['city'] == '')
+						{
+							$data['city'] = 'na';
+						}
+						if ($data['state'] == '')
+						{
+							$data['state'] = 'na';
+						}
+						if ($data['country'] == '')
+						{
+							$data['country'] = 'US';
+						}
+						if (trim($data['company']) == '')
+						{
+							$org = $data['name'];
+						}
+						else
+						{
+							$org = $data['company'];
+						}
+						if (trim($org) == '')
+						{
+							$org = 'Account' . $data['id'];
+						}
+						if ($extra['private_whois'] == 1)
+						{
+//							$org = 'Private Customer';
+//							$data['address1'] = 'Private Residence';
+						}
+						
+						$found = true;
+						$cmds .= 'cd /opt/rwhoisd/etc/rwhoisd/net-' . $ipblock_ip . '-' . $ipblock_size . ';\n'
+						. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+						. 'Auth-Area: ' . $ipblock . '\n'
+						. 'Org-Name: ' . trim($org) . '\n'
+						. 'Street-Address: ' . trim($data['address1']) . '\n'
+						. 'City: ' . $data['city'] . '\n'
+						. 'State: ' . $data['state'] . '\n'
+						. 'Postal-Code: ' . $data['zipcode'] . '\n'
+						. 'Country-Code: ' . $data['country'] . '\n'
+						. 'Phone: ' . trim($data['phone']) . '\n'
+						. 'Created: 20050101\n'
+						. 'Updated: ' . date('Ymd') . '" > data/org/' . $data['id'] . '.txt;\n'
+						. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+						. 'Auth-Area: ' . $ipblock . '\n'
+						. 'Network-Name: INTSRV-' . $ip . '\n'
+						. 'IP-Network: ' . $ip . '/' . $size . '\n'
+						. 'Org-Name: ' . trim($org) . '\n'
+						. 'Street-Address: ' . $data['address1'] . '\n'
+						. 'City: ' . $data['city'] . '\n'
+						. 'State: ' . $data['state'] . '\n'
+						. 'Postal-Code: ' . $data['zipcode'] . '\n'
+						. 'Country-Code: ' . $data['country'] . '\n'
+						. 'Created: 20050101\n'
+						. 'Updated: ' . date('Ymd') . '\n'
+						. 'Updated-By: abuse@interserver.net" > data/network/' . $ip . '-' . $size . '.txt;\n';
+					}
+				}
+				else
+				{
+					$query = "select * from servers where servers_hostname like '%$server%' or servers_hostname='$server'"; 
+					$db4->query($query);
+					if ($db4->num_rows() == 0)
+					{
+						$group = 162;
+					}
+					else
+					{
+						$db4->next_record();
+						$serverinfo = $db4->Record;
+						$groups = explode(':', $serverinfo['servers_group']);
+						$group = $groups[1];
+						if ($group == '')
+						{
+							$group = 162;
+						}
+					}
+							$data = $GLOBALS['tf']->accounts->read($group);
+							$data['url'] = str_replace(array(' ', ','), array('_', ''), $data['url']);
+							if ($data['address'] == '')
+							{
+								$data['address'] = 'na';
+							}
+							if ($data['city'] == '')
+							{
+								$data['city'] = 'na';
+							}
+							if ($data['state'] == '')
+							{
+								$data['state'] = 'na';
+							}
+							if ($data['country'] == '')
+							{
+								$data['country'] = 'US';
+							}
+							if (trim($data['url']) == '')
+							{
+								$org = $data['account_lid'];
+							}
+							else
+							{
+								$org = $data['url'];
+							}
+							if (trim($org) == '')
+							{
+								$org = 'Account' . $group;
+							}
+							$found = true;
+							$cmds .= 'cd /opt/rwhoisd/etc/rwhoisd/net-' . $ipblock_ip . '-' . $ipblock_size . ';\n'
+							. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+							. 'Auth-Area: ' . $ipblock . '\n'
+							. 'Org-Name: ' . trim($org) . '\n'
+							. 'Street-Address: ' . trim($data['address']) . '\n'
+							. 'City: ' . $data['city'] . '\n'
+							. 'State: ' . $data['state'] . '\n'
+							. 'Postal-Code: ' . $data['zip'] . '\n'
+							. 'Country-Code: ' . $data['country'] . '\n'
+							. 'Phone: ' . trim($data['phone']) . '\n'
+							. 'Created: 20050101\n'
+							. 'Updated: ' . date('Ymd') . '" > data/org/' . $data['url'] . '.txt;\n'
+							. 'echo -e "ID: NETBLK-INTSRV.' . $ipblock . '\n'
+							. 'Auth-Area: ' . $ipblock . '\n'
+							. 'Network-Name: INTSRV-' . $ip . '\n'
+							. 'IP-Network: ' . $ip . '/' . $size . '\n'
+							. 'Org-Name: ' . trim($org) . '\n'
+							. 'Street-Address: ' . $data['address'] . '\n'
+							. 'City: ' . $data['city'] . '\n'
+							. 'State: ' . $data['state'] . '\n'
+							. 'Postal-Code: ' . $data['zip'] . '\n'
+							. 'Country-Code: ' . $data['country'] . '\n'
+							. 'Created: 20050101\n'
+							. 'Updated: ' . date('Ymd') . '\n'
+							. 'Updated-By: abuse@interserver.net" > data/network/' . $ip . '-' . $size . '.txt;\n';
+/*
+						}
+						else
+						{
+							//echo "Couldnt Find Group For Comment/Server/Group $server/" . $serverinfo['servers_hostname'] . "/$group\n";
+						}
+					}
+					else
+					{
+						//echo "Cant Find Server $server\n";
+					}
+*/
+/**/
+				}
+		}
+/**/
+		echo str_replace('\n', "\n", $cmds);
+		if ($found === false)
+		{
+			echo "echo \"Cant Find VLAN $ipblock\";\n";
+			
+		}
+	}
+//	echo "$total\n";
+?>
