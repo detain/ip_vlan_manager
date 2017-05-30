02d1eec7b0166c4837a1bb702f20aa9a1868ea66
detain <detain@b7b71f07-cbf7-474a-b9d0-0f5a9ada8a20>
2017-05-30 19:16:19 +0000

    lots of updates to ip vlan manager code splitting off a bunch of the pages off into individual files

---
+++ trunk/vlan_edit_port.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * IP Functionality
+ *
+ * Last Changed: $LastChangedDate: 2017-05-26 04:36:01 -0400 (Fri, 26 May 2017) $
+ * @author detain
+ * @version $Revision: 24803 $
+ * @copyright 2017
+ * @package MyAdmin
+ * @category IPs
+ */
+
