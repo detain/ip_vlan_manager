7c670c6da8d4912a9ca2acaefa26d7edfaf881c5
Joe Huss <detain@interserver.net>
2018-03-30 15:01:40 -0400

    Updated vlan_edit_port so it should update the switchports table now as well as updating the vlans table , and when called from the view server page it also updates the server_id in switchports table

--- src/vlan_edit_port.php
+++ src/vlan_edit_port.php
@@ -33,8 +33,9 @@ function vlan_edit_port() {
 	$table->add_row();
 	$db->query("select * from vlans where vlans_networks like '%:{$ipblock}:%'");
 	$db->next_record();
+	$vlanInfo = $db->Record;
 	function_requirements('get_networks');
-	$networks = get_networks($db->Record['vlans_networks'], $db->Record['vlans_id'], $db->Record['vlans_comment'], $db->Record['vlans_ports']);
+	$networks = get_networks($vlanInfo['vlans_networks'], $vlanInfo['vlans_id'], $vlanInfo['vlans_comment'], $vlanInfo['vlans_ports']);
 	$networksize = count($networks);
 	$rows = [];
 	for ($x = 0; $x < $networksize; $x++) {
@@ -67,9 +68,53 @@ function vlan_edit_port() {
 		$table->add_field($select.'<br>'.$table->make_submit('Set Port(s)'));
 		$editport = TRUE;
 	} else {
-		$ports = ':'.implode(':', $GLOBALS['tf']->variables->request['ports']).':';
-		$query = "update vlans set vlans_ports='{$ports}' where vlans_networks like '%:{$network}:%' and vlans_id='{$vlan}'";
-		$db2->query($query, __LINE__, __FILE__);
+		$orig_switchports = [];
+		$switchports = [];
+		$new_ports = $GLOBALS['tf']->variables->request['ports'];
+		$asset_id = null;
+		$server_id = null;
+		if (isset($GLOBALS['tf']->variables->request['pop_order_id']))
+			$server_id = $GLOBALS['tf']->variables->request['pop_order_id'];
+		foreach ($new_ports as $switchport) {
+			list($switch, $port, $blade, $justport) = parse_vlan_ports($switchport);
+			$db2->query("select * from switchports where switch='{$switch}' and port='{$port}'", __LINE__, __FILE__);
+			if ($db2->num_rows() > 0) {
+				$db2->next_record(MYSQL_ASSOC);
+				$switchports[$db2->Record['switchport_id']] = $db2->Record;
+			}
+		}
+		foreach ($ports as $switchport) {
+			list($switch, $port, $blade, $justport) = parse_vlan_ports($switchport);
+			$db2->query("select * from switchports where switch='{$switch}' and port='{$port}'", __LINE__, __FILE__);
+			if ($db2->num_rows() > 0) {
+				$db2->next_record(MYSQL_ASSOC);
+				$orig_switchports[$db2->Record['switchport_id']] = $db2->Record;
+				if (!in_array($db2->Record['switchport_id'], array_keys($switchports))) {
+					$vlans = explode(',', $db2->Record['vlans']);
+					if (($key = array_search($vlanInfo['vlans_id'], $vlans)) !== false)
+						unset($vlans[$key]);
+					$vlans = implode(',', $vlans);
+					if (!is_null($server_id))
+						$db2->query("update switchports set vlans='{$vlans}', server_id=null, asset_id=null where switchport_id={$db2->Record['switchport_id']}", __LINE__, __FILE__);
+					else
+						$db2->query("update switchports set vlans='{$vlans}' where switchport_id={$db2->Record['switchport_id']}", __LINE__, __FILE__);
+				}
+			}
+		}
+		foreach ($switchports as $switchport_id => $switchportData) {
+			if (!in_array($switchport_id, array_keys($orig_switchports)) || !is_null($server_id)) {
+				$vlans = explode(',', $switchportData['vlans']);
+				if (!in_array($vlanInfo['vlans_id'], $vlans))
+					$vlans[] = $vlanInfo['vlans_id'];
+				$vlans = implode(',', $vlans);
+				if (!is_null($server_id))
+					$db2->query("update switchports set vlans='{$vlans}',server_id={$server_id} where switchport_id={$db2->Record['switchport_id']}", __LINE__, __FILE__);
+				else
+					$db2->query("update switchports set vlans='{$vlans}' where switchport_id={$db2->Record['switchport_id']}", __LINE__, __FILE__);
+			}
+		}
+		$db2->query("update vlans set vlans_ports=':".implode(':', $new_ports).":' where vlans_networks like '%:{$network}:%' and vlans_id='{$vlan}'", __LINE__, __FILE__);
+
 		//function_requirements('update_switch_ports');
 		//update_switch_ports();
 		if(isset($GLOBALS['tf']->variables->request['source']) && $GLOBALS['tf']->variables->request['source'] == 'popup_order') {
