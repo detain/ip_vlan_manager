9760f13925c3d05a550a6dfc54224c0eff87506f
Joe Huss <detain@interserver.net>
2024-09-11 14:16:57 -0400

    udpates to the rwhois generator fixingf up dir creations , serial fixes and other improfvements.  its mostly done now

--- src/setup_rwhois.php
+++ src/setup_rwhois.php
@@ -3,10 +3,13 @@
 * VLAN to rWhois Generator
 * 
 * @todo
-* * use real or private contact info for each user based on account setting
-* if you want i could add vps network definitions as well
+* - iterate vlans and genrate nets/orgs
+* - use real or private contact info for each user based on account setting
+* ? if you want i could add vps network definitions as well
+* 
 */
-echo "Loading rwhoisd JSON Data\n";
+
+//echo "Loading rwhoisd JSON Data\n";
 if (file_exists('rwhoisd.json')) {
     $json = file_get_contents('rwhoisd.json');    
 } else {
@@ -16,7 +19,6 @@ if (file_exists('rwhoisd.json')) {
 $json = json_decode($json, true);
 $samplePrefix = 'https://raw.githubusercontent.com/arineng/rwhoisd/master/rwhoisd/sample.data/';
 $typeDirs = ['domain' => 'a.com', 'net' => 'net-fd00%3A1234%3A%3A-32'];
-$domains = ['interserver.net'];
 $rwhoisFiles = ['allow', 'conf', 'deny', 'dir', 'root', 'x.dir'];
 $defs = [ 'domain' => ['asn', 'contact', 'domain', 'guardian', 'host', 'org', 'referral'], 'net' => ['contact', 'guardian', 'host', 'network', 'referral'] ];
 $templates = ['domain' => [], 'net' => []];
@@ -25,8 +27,8 @@ $installDir = '/home/rwhois/bin';
 $installDir = '/home/rwhois/bin/new';
 $serial = date('YmdHis');
 $privateData = true;
-echo "Setting up rwhoisd in {$installDir}\n";
-echo "Downloading Templates\n";
+//echo "Setting up rwhoisd in {$installDir}\n";
+//echo "Downloading Templates\n";
 foreach ($rwhoisFiles as $file) {
     $templates['rwhoisd.'.$file] = file_get_contents($samplePrefix.'rwhoisd.'.$file);
 }
@@ -35,8 +37,8 @@ foreach ($defs as $defType => $typeDefs) {
         $templates[$defType][$def] = file_get_contents($samplePrefix.$typeDirs[$defType].'/attribute_defs/'.$def.'.tmpl');
     }
 }
-@mkdir($installDir, 0774, true);
-echo "Generating Output\n";
+//mkdir($installDir, 0774, true);
+//echo "Generating Output\n";
 $out = [
     'domains' => [],
     'nets' => [],
@@ -50,7 +52,7 @@ Time-To-Live:{$intervals['ttl']}
 Primary-Server:rwhois.trouble-free.net:4321
 Hostmaster:hostmaster@interserver.net",
 ];
-echo "Writing IP Blocks\n";
+//echo "Writing IP Blocks\n";
 // generate output from data
 foreach ($json['ipblocks'] as $blockType => $typeBlocks) {
     foreach ($typeBlocks as $blockId => $blockData) {
@@ -60,17 +62,18 @@ foreach ($json['ipblocks'] as $blockType => $typeBlocks) {
         // $contact = $custid;
         $netDir = 'net-'.str_replace('/', '-', $ipblock);
         $netName = 'NETBLK-'.$ipblock;
-        @mkdir($installDir.'/'.$netDir, 0774, true);
+        //mkdir($installDir.'/'.$netDir, 0774, true);
         $out['authArea'][] = "type: master
 name: {$netName}
 data-dir: {$netDir}/data
 schema-file: {$netDir}/schema
 soa-file: {$netDir}/soa";
-        @mkdir($installDir.'/'.$netDir.'/attribute_defs', 0774, true);
+        //mkdir($installDir.'/'.$netDir.'/attribute_defs', 0774, true);
         foreach ($defs['net'] as $suffix) {
-            @mkdir($installDir.'/'.$netDir.'/data/'.$suffix, 0774, true);
+            //mkdir($installDir.'/'.$netDir.'/data/'.$suffix, 0774, true);
+            echo `mkdir -p "{$installDir}/{$netDir}/data/{$suffix}"`;
         }
-        $out['networks'][] = "Network-Name: NETBLK-{$ipblock}
+        $networks[] = "Network-Name: NETBLK-{$ipblock}
 IP-Network: {$ipblock}
 Organization: 777.interserver.net
 Tech-Contact: hostmaster.interserver.net
@@ -79,20 +82,42 @@ Admin-Contact: {$contact}.interserver.net";
 attributedef:{$netDir}/attribute_defs/network.tmpl
 dbdir:{$netDir}/data/network
 Schema-Version: {$serial}";
+        foreach ($blockData['vlans'] as $vlanId => $vlan) {
+            if ($blockType == 4) {
+                $ipblock = $vlan['vlans_networks'];
+            } else {
+                $ipblock = $vlan['vlans6_networks'];
+            }
+            $contact = 'hostmaster';
+            if (!is_null($vlan['account_id'])) {
+                $contact = $vlan['account_id'];                
+            }
+            $netDir = 'net-'.str_replace('/', '-', $ipblock);
+            $netName = 'NETBLK-'.$ipblock;
+            $networks[] = "Network-Name: NETBLK-{$ipblock}
+IP-Network: {$ipblock}
+Organization: 777.interserver.net
+Tech-Contact: hostmaster.interserver.net
+Admin-Contact: {$contact}.interserver.net";            
+        }
+        echo `mkdir -p "{$installDir}/{$netDir}/data/referral" "{$installDir}/{$netDir}/data/network" "{$installDir}/{$netDir}/attribute_defs"`;
         // write net schema file
         file_put_contents($installDir.'/'.$netDir.'/schema', $schema);
         // write net soa file
         file_put_contents($installDir.'/'.$netDir.'/soa', $out['soa']);
         // write net network.txt
         file_put_contents($installDir.'/'.$netDir.'/data/referral/referral.txt', '');
+        file_put_contents($installDir.'/'.$netDir.'/data/network/network.txt', implode("\n---\n", $networks));        
     }
 }
-echo "Writiong Domains\n";
+//echo "Writing Domains\n";
 foreach ($json['domains'] as $domain => $domData) {
+    echo `mkdir -p "{$installDir}/{$domain}/attribute_defs"`;
     // create attribute_defs and assorted data dirs
-    @mkdir($installDir.'/'.$domain.'/attribute_defs', 0774, true);
+    //mkdir($installDir.'/'.$domain.'/attribute_defs', 0774, true);
     foreach ($defs['domain'] as $suffix) {
-        @mkdir($installDir.'/'.$domain.'/data/'.$suffix, 0774, true);
+        //mkdir($installDir.'/'.$domain.'/data/'.$suffix, 0774, true);
+        echo `mkdir -p "{$installDir}/{$domain}/data/{$suffix}"`;
     }
     $out['authArea'][] = "type: master
     name: {$domain}
@@ -156,7 +181,7 @@ Organization:777.{$domain}
 Admin-Contact:222.{$domain}
 Tech-Contact:222.{$domain}
 Created:{$serial}
-Updated:19961023
+Updated:{$serial}
 Updated-by:hostmaster@{$domain}";
     $contacts[] = "ID:222.{$domain}
 Auth-Area:{$domain}
@@ -170,9 +195,9 @@ Fax:(847)-338-0340
 Organization:777.{$domain}
 See-Also:http://www.{$domain}/~johnq
 Created:{$serial}
-Updated:11961023
+Updated:{$serial}
 Updated-By:hostmaster@{$domain}";
-    $domain = "ID:333.{$domain}
+    $domainData = "ID:333.{$domain}
 Auth-Area:{$domain}
 Guardian:444.{$domain}
 Domain-Name: {$domain}
@@ -183,14 +208,14 @@ Admin-Contact:222.{$domain}
 Tech-Contact:222.{$domain}
 Billing-Contact:222.{$domain}
 Created:{$serial}
-Updated:19961023
+Updated:{$serial}
 Updated-By:hostmaster@{$domain}";
     $guardian = "ID: 444.{$domain}
 Auth-Area: {$domain}
 Guard-Scheme: PW
 Guard-Info: passwd
 Created: {$serial}
-Updated: 19961023
+Updated: {$serial}
 Updated-By: hostmaster@{$domain}
 Private:true";
     $host[] = "ID: 444.{$domain}
@@ -198,7 +223,7 @@ Auth-Area: {$domain}
 Guard-Scheme: PW
 Guard-Info: passwd
 Created: {$serial}
-Updated: 19961023
+Updated: {$serial}
 Updated-By: hostmaster@{$domain}
 Private:true";
     $org = "ID: 777.{$domain}
@@ -211,7 +236,7 @@ Postal-Code: V6Z 2H3
 Country-Code: NL
 Phone: (401) 555-6721
 Created: {$serial}
-Updated: 19961023
+Updated: {$serial}
 Updated-By: hostmaster@{$domain}";
     $referral = "ID:888.{$domain}
 Auth-Area: {$domain}
@@ -220,14 +245,14 @@ Referral:rwhois://rwhois.second.{$domain}:4321/Auth-Area=fddi.{$domain}
 Organization:777.{$domain}
 Referred-Auth-Area:fddi.{$domain}
 Created:{$serial}
-Updated:19961023
+Updated:{$serial}
 Updated-By:hostmaster@{$domain}";                             
     file_put_contents($installDir.'/'.$domain.'/data/asn/asn.txt', $asn);
-    file_put_contents($installDir.'/'.$domain.'/data/contacts/contacts.txt', implode("\n---\n", $contacts));
-    file_put_contents($installDir.'/'.$domain.'/data/domain/domain.txt', $domain);
+    file_put_contents($installDir.'/'.$domain.'/data/contact/contact.txt', implode("\n---\n", $contacts));
+    file_put_contents($installDir.'/'.$domain.'/data/domain/domain.txt', $domainData);
     file_put_contents($installDir.'/'.$domain.'/data/guardian/guardian.txt', $guardian);
     file_put_contents($installDir.'/'.$domain.'/data/host/host.txt', implode("\n---\n", $contacts));
-    file_put_contents($installDir.'/'.$domain.'/data/org/orgl.txt', $org);
+    file_put_contents($installDir.'/'.$domain.'/data/org/org.txt', $org);
     file_put_contents($installDir.'/'.$domain.'/data/referral/referral.txt', $referral);
     foreach ($defs as $defType => $typeDefs) {
         foreach ($typeDefs as $def) {
@@ -235,7 +260,7 @@ Updated-By:hostmaster@{$domain}";
         }
     }
 }
-file_put_contents($installDir.'/rwhoisd.auth_area', implode("\n---\n", $authArea));
+file_put_contents($installDir.'/rwhoisd.auth_area', implode("\n---\n", $out['authArea']));
 foreach ($rwhoisFiles as $file) {
     file_put_contents($installDir.'/rwhoisd.'.$file, $templates['rwhoisd.'.$file]);
 }
