fc1e7142f8755e57b818203c7a117f073a24dab8
Joe Huss <detain@interserver.net>
2025-01-07 05:09:23 -0500

    rewrite to update switch ports to utilize direct queries with teh cacti db instead of servermap url

--- src/update_switch_ports.php
+++ src/update_switch_ports.php
@@ -8,108 +8,90 @@
  */
 function update_switch_ports($verbose = false, $pullServerMap = true)
 {
+/*
+*/
     $db = get_module_db('default');
-    $db2 = clone $db;
-    if ($pullServerMap !== false) {
-        $vlan_ids = [];
-        $switch_ids = [];
-        $db->query("select vlans_id from vlans");
-        while ($db->next_record(MYSQL_ASSOC)) {
-            $vlan_ids[$db->Record['vlans_id']] = true;
-        }
-        $lines = explode("\n", trim(getcurlpage('https://nms.is.cc/cacti/servermap.php')));
-        $switches = [];
-        foreach ($lines as $line) {
-            [$graph_id, $switch, $port, $comment] = explode(',', $line);
-            if ($switch != '') {
-                $switches[$switch][$port] = $graph_id;
-            }
-        }
-        foreach ($switches as $switch => $ports) {
-            $foundports = [];
-            $db->query("select * from switchmanager where name='{$switch}'", __LINE__, __FILE__);
-            if ($db->num_rows() == 0) {
-                $db->query(make_insert_query('switchmanager', [
-                    'id' => null,
-                    'name' => $switch,
-                    'ports' => count($ports),
-                    'updated' => mysql_now(),
-                ]), __LINE__, __FILE__);
-                if ($verbose == true) {
-                    add_output("Created New Switch {$switch} - ");
-                }
-                $db->query("select * from switchmanager where name='{$switch}'", __LINE__, __FILE__);
-            }
-            $db->next_record(MYSQL_ASSOC);
-            $switchManager = $db->Record;
-            $db->query("update switchmanager set updated=now() where id={$switchManager['id']}", __LINE__, __FILE__);
-            $switch_ids[] = $switchManager['id'];
-            if ($verbose == true) {
-                add_output("Loaded Switch {$switch} - ");
-            }
-            foreach ($ports as $port => $graph_id) {
-                $blade = '';
-                $justport = $port;
-                if (mb_strrpos($port, '/') > 0) {
-                    $blade = mb_substr($port, 0, mb_strrpos($port, '/'));
-                    $justport = mb_substr($port, mb_strlen($blade) + 1);
-                }
-                if (isset($foundports[$justport])) {
-                    $justport = '';
-                } else {
-                    $foundports[$justport] = true;
-                }
-                $db->query("select * from switchports where switch='{$switchManager['id']}' and port='{$port}'", __LINE__, __FILE__);
-                if ($db->num_rows() == 0) {
-                    if ($verbose == true) {
-                        add_output("{$port} +");
-                    }
-                    $db->query(make_insert_query('switchports', [
-                        'switch' => $switchManager['id'],
-                        'blade' => $blade,
-                        'justport' => $justport,
-                        'port' => $port,
-                        'graph_id' => $graph_id,
-                        'vlans' => '',
-                        'asset_id' => 0,
-                        'updated' => mysql_now(),
-                    ]), __LINE__, __FILE__);
-                } else {
-                    $db->next_record();
-                    if (($db->Record['blade'] != $blade) || ($db->Record['justport'] != $justport)) {
-                        if ($verbose == true) {
-                            add_output("\nUpdate BladePort");
-                        }
-                        $query = "update switchports set blade='{$blade}', justport='{$justport}' where switch='{$switchManager['id']}' and port='{$port}'";
-                        //echo $query;
-                        $db->query($query, __LINE__, __FILE__);
-                    }
-                    if ($verbose == true) {
-                        add_output("$port ");
-                    }
-                    if ($db->Record['graph_id'] != $graph_id) {
-                        if ($verbose == true) {
-                            add_output("\nUpdate Graph");
-                        }
-                        $query = "update switchports set graph_id='{$graph_id}' where switch='{$switchManager['id']}' and port='{$port}'";
-                        //echo $query;
-                        $db->query($query, __LINE__, __FILE__);
-                    }
-                    $db->query("update switchports set updated=now() where switchport_id={$db->Record['switchport_id']}", __LINE__, __FILE__);
-                    if ($verbose == true) {
-                        add_output("$graph_id ");
-                    }
-                }
-                if ($verbose == true) {
-                    add_output(',');
-                }
-            }
-            if ($verbose == true) {
-                add_output("\n");
-            }
-        }
-        add_output(sizeof(array_keys($vlan_ids)).' Unmatched VLANs'.PHP_EOL);
+    $db->query("select id, name from switchmanager", __LINE__, __FILE__);
+    $local = [];
+    while ($db->next_record(MYSQL_ASSOC)) {
+        $local[$db->Record['name']] = ['id' => $db->Record['id'], 'ports' => []];
+        $localIdToName[$db->Record['id']] = $db->Record['name'];
     }
+    $db->query("select switch, switchport_id, port, graph_id from switchports", __LINE__, __FILE__);
+    while ($db->next_record(MYSQL_ASSOC)) {
+        $local[$localIdToName[$db->Record['switch']]]['ports'][$db->Record['port']] = ['id' => $db->Record['switchport_id'], 'graph_id' => $db->Record['graph_id']];
+    }
+    $db2 = new \MyDb\Mysqli\Db('cacti', SNMP_MYSQL_USER, SNMP_MYSQL_PASS, SNMP_SSH_HOST);
+    $db2->query("select local_graph_id as graph_id,description as switch,field_value as port from graph_templates_graph, graph_local, host, host_snmp_cache where local_graph_id=graph_local.id and graph_local.host_id=host.id and graph_local.host_id=host_snmp_cache.host_id and graph_local.snmp_index=host_snmp_cache.snmp_index and field_name='ifName' order by description, field_value", __LINE__, __FILE__);
+    $nowUpdates = [];
+    $nowIdx = 0;
+    while ($db2->next_record(MYSQL_ASSOC)) {
+        if (!isset($local[$db2->Record['switch']])) {
+            $db->query(make_insert_query('switchmanager', [
+                'id' => null,
+                'name' => $db2->Record['switch'],
+                'ports' => count($ports),
+                'updated' => mysql_now(),
+            ]), __LINE__, __FILE__);
+            $id = $db->getLastInsertId('switchmanager', 'id');
+            $local[$db2->Record['switch']] = ['id' => $id, 'ports' => []];
+            $localIdToName[$id] = $db2->Record['switch'];
+            if ($verbose == true) {
+                add_output("Created New Switch {$db2->Record['switch']} - ");
+            }
+        } else {
+            if ($verbose == true) {
+                add_output("Loaded Switch {$db2->Record['switch']} - ");
+            }
+            $now = mysql_now();
+            $db->query("update switchmanager set updated='{$now}' where id={$local[$db2->Record['switch']]['id']}", __LINE__, __FILE__);
+        }
+        if (!isset($local[$db2->Record['switch']]['ports'][$db2->Record['port']])) {
+            if (substr($db2->Record['port'], 0, 4) == 'Vlan') {
+                continue;
+            } 
+            $blade = '';
+            $justport = $db2->Record['port'];
+            if (mb_strrpos($db2->Record['port'], '/') > 0) {
+                $blade = mb_substr($db2->Record['port'], 0, mb_strrpos($db2->Record['port'], '/'));
+                $justport = mb_substr($db2->Record['port'], mb_strlen($blade) + 1);
+            }
+            if ($verbose == true) {
+                add_output("{$db2->Record['port']} +");
+            }
+            $db->query(make_insert_query('switchports', [
+                'switch' => $local[$db2->Record['switch']]['id'],
+                'blade' => $blade,
+                'justport' => $justport,
+                'port' => $db2->Record['port'],
+                'graph_id' => $db2->Record['graph_id'],
+                'vlans' => '',
+                'asset_id' => 0,
+                'updated' => mysql_now(),
+            ]), __LINE__, __FILE__);
+            $id = $db->getLastInsertId('switchports', 'switchport_id');
+            $local[$db2->Record['switch']]['ports'][$db2->Record['port']] = ['id' => $id, 'graph_id' => $db2->Record['graph_id']];            
+        } elseif ($local[$db2->Record['switch']]['ports'][$db2->Record['port']]['graph_id'] != $db2->Record['graph_id']) {
+            $now = mysql_now();
+            $db->query("update switchports set updated='{$now}', graph_id={$db2->Record['graph_id']} where switchport_id={$local[$db2->Record['switch']]['ports'][$db2->Record['port']]['id']}", __LINE__, __FILE__);
+            if ($verbose == true) {
+                add_output("{$db2->Record['port']} Update Graph {$db2->Record['graph_id']}\n");
+            }            
+        } else {
+            $nowUpdates[] = $local[$db2->Record['switch']]['ports'][$db2->Record['port']]['id'];
+            $nowIdx++;
+            if ($nowIdx > 1000) {
+                $now = mysql_now();
+                $db->query("update switchports set updated='{$now}' where switchport_id in (".implode(',', $nowUpdates).")", __LINE__, __FILE__);
+                $nowUpdates = [];
+                $nowIdx = 0;                
+            } 
+        }
+    }
+    if ($nowIdx > 0) {
+        $now = mysql_now();
+        $db->query("update switchports set updated='{$now}' where switchport_id in (".implode(',', $nowUpdates).")", __LINE__, __FILE__);
+    } 
     global $output;
     echo str_replace("\n", "<br>\n", $output);
     $output = '';
